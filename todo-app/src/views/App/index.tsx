e92740d072cd87186a27558c92d907e30f5df9af
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2022-04-26 01:58:41 +0300

    init

---
+++ todo-app/src/views/App/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const App: React.FC = () => {
+    return (
+        <article>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio, quam maxime repellat expedita velit pariatur consequatur soluta sint temporibus error similique rerum animi numquam, quibusdam, a ipsam est fuga. Quidem!</article>
+    )
+
+
+}
